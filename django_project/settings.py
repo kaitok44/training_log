cc93deed0f50e46a4c1857d5927442ca4972f299
kaitok44 <kaito.2151@gmail.com>
2025-01-02 17:20:18 +0900

    add pages app

--- django_project/settings.py
+++ django_project/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "crispy_bootstrap5",
     # Local
     "accounts",
+    "pages",
+    "articles",
 ]
 
 MIDDLEWARE = [
@@ -110,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Tokyo"
 
 USE_I18N = True
 
