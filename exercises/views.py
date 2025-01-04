bc68a294b271714b9266741f6a1ba5e1dbcc882f
kaitok44 <kaito.2151@gmail.com>
2025-01-04 15:47:06 +0900

    permissions and authorizations for exercise app

--- exercises/views.py
+++ exercises/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+)
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import (
     UpdateView, DeleteView, CreateView
@@ -5,15 +9,21 @@ from django.views.generic.edit import (
 from django.urls import reverse_lazy
 from .models import Exercise
 
-class ExerciseListView(ListView):
+class ExerciseListView(LoginRequiredMixin, ListView):
     model = Exercise
     template_name = "exercise_list.html"
 
-class ExerciseDetailView(DetailView):
+class ExerciseDetailView(
+    LoginRequiredMixin, UserPassesTestMixin, DetailView):
     model = Exercise
     template_name = "exercise_detail.html"
 
-class ExerciseUpdateView(UpdateView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ExerciseUpdateView(
+    LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Exercise
     fields = (
         "exercise_name",
@@ -21,17 +31,30 @@ class ExerciseUpdateView(UpdateView):
     )
     template_name = "exercise_edit.html"
 
-class ExerciseDeleteView(DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ExerciseDeleteView(
+    LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Exercise
     template_name = "exercise_delete.html"
     success_url = reverse_lazy("exercise_list")
 
-class ExerciseCreateView(CreateView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ExerciseCreateView(LoginRequiredMixin, CreateView):
     model = Exercise
     template_name = "exercise_new.html"
     fields = (
         "exercise_name",
         "muscles_worked",
-        "author",
     )
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
 # Create your views here.
